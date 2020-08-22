0887250e9e153b95b310bb448c16f04735a2f2c8
dododome01 <dododome01@gmail.com>
2020-08-22 03:00:20 +0200

    Add Saplings wither in Desert

---
+++ src/main/java/com/github/dododome01/mixin/SaplingBlockMixin.java
@@ -0,0 +1,31 @@
+package com.github.dododome01.mixin;
+
+import com.github.dododome01.helper.BlockSaplingHelper;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SaplingBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(SaplingBlock.class)
+public abstract class SaplingBlockMixin
+{
+    @Inject(method = "generate", at = @At(value = "INVOKE", shift = At.Shift.BEFORE,
+            target = "Lnet/minecraft/block/sapling/SaplingGenerator;generate(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Ljava/util/Random;)Z"),
+            cancellable = true)
+    private void onGenerate(ServerWorld serverWorld_1, BlockPos blockPos_1, BlockState blockState_1, Random random_1, CallbackInfo ci)
+    {
+        if(serverWorld_1.getBiome(blockPos_1).getCategory() == Biome.Category.DESERT && !BlockSaplingHelper.hasWater(serverWorld_1, blockPos_1))
+        {
+            serverWorld_1.setBlockState(blockPos_1, Blocks.DEAD_BUSH.getDefaultState(), 3);
+            ci.cancel();
+        }
+    }
+}
