8576ca8c9d9122e25e7b98489ec9befca7b23cae
dododome01 <dododome01@gmail.com>
2020-08-22 03:05:59 +0200

    Add renewable lava

---
+++ src/main/java/com/github/dododome01/helper/ObsidianBlock.java
@@ -0,0 +1,34 @@
+package com.github.dododome01.helper;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+import java.util.Random;
+
+public class ObsidianBlock extends Block {
+    public ObsidianBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        for (Direction dir : Direction.values()) {
+            FluidState neighbor = world.getFluidState(pos.offset(dir));
+            if (neighbor.getFluid() != Fluids.LAVA || !neighbor.isStill()) return;
+        }
+        if (random.nextInt(10) == 0) {
+            world.setBlockState(pos, Blocks.LAVA.getDefaultState());
+        }
+    }
+}
