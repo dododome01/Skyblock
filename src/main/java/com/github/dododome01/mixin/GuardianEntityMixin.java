dc15e7ccbaf1f6e1d043b434fd62deb3c50a5ebd
dododome01 <dododome01@gmail.com>
2020-08-22 02:57:06 +0200

    Add Guardian transformation

---
+++ src/main/java/com/github/dododome01/mixin/GuardianEntityMixin.java
@@ -0,0 +1,47 @@
+package com.github.dododome01.mixin;
+
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.ElderGuardianEntity;
+import net.minecraft.entity.mob.GuardianEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GuardianEntity.class)
+public abstract class GuardianEntityMixin extends HostileEntity
+{
+    protected GuardianEntityMixin(EntityType<? extends HostileEntity> entityType_1, World world_1)
+    {
+        super(entityType_1, world_1);
+    }
+
+    @Override
+    public void onStruckByLightning(ServerWorld serverWorld, LightningEntity lightningEntity)
+    {
+        if (!this.world.isClient && !this.removed  && !((Object)this instanceof ElderGuardianEntity))
+        {
+            ElderGuardianEntity elderGuardian = new ElderGuardianEntity(EntityType.ELDER_GUARDIAN ,this.world);
+            elderGuardian.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.yaw, this.pitch);
+            elderGuardian.initialize(serverWorld ,this.world.getLocalDifficulty(elderGuardian.getBlockPos()), SpawnReason.CONVERSION, (EntityData)null, (CompoundTag)null);
+            elderGuardian.setAiDisabled(this.isAiDisabled());
+
+            if (this.hasCustomName())
+            {
+                elderGuardian.setCustomName(this.getCustomName());
+                elderGuardian.setCustomNameVisible(this.isCustomNameVisible());
+            }
+
+            this.world.spawnEntity(elderGuardian);
+            this.remove();
+        }
+        else
+        {
+            super.onStruckByLightning(serverWorld, lightningEntity);
+        }
+    }
+}
